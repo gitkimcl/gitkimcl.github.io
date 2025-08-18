5cbb4353abef821022a059f038ff1d87d1732835
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-08-18 23:25:24 +0900

    add notfont(cycscript transcriber)

---
+++ things/notfont/script.js
@@ -0,0 +1,132 @@
+function switch1() {
+    code = JSON.parse(`{ "r":[{"f":"ㄱ"}], "s":[{"f":"ㄴ"}], "e":[{"f":"mod1"},{"f":"ㄴ"}], "f":[{"f":"ㄹ"}], "a":[{"f":"ㅁ"}], "q":[{"f":"mod1"},{"f":"ㅁ"}], "t":[{"f":"ㅅ"}], "d":[{"f":"ㅇ"}], "w":[{"f":"mod1"},{"f":"ㅅ"}], "c":[{"f":"mod0"},{"f":"ㅅ"}], "z":[{"f":"mod1"},{"f":"ㄱ"}], "x":[{"f":"mod0"},{"f":"ㄴ"}], "v":[{"f":"mod0"},{"f":"ㅁ"}], "g":[{"f":"mod1"},{"f":"ㅇ"}], "R":[{"f":"mod2"},{"f":"ㄱ"}], "E":[{"f":"mod3"},{"f":"ㄴ"}], "Q":[{"f":"mod3"},{"f":"ㅁ"}], "T":[{"f":"mod2"},{"f":"ㅅ"}], "W":[{"f":"mod3"},{"f":"ㅅ"}], "k":[{"f":"ㅏ"}], "i":[{"f":"mod2"},{"f":"ㅏ"}], "j":[{"f":"ㅓ"}], "u":[{"f":"mod2"},{"f":"ㅓ"}], "h":[{"f":"ㅗ"}], "y":[{"f":"mod2"},{"f":"ㅗ"}], "n":[{"f":"ㅜ"}], "b":[{"f":"mod2"},{"f":"ㅜ"}], "m":[{"f":"ㅡ"}], "l":[{"f":"ㅣ"}], "o":[{"f":"ㅐ"}], "O":[{"f":"mod2"},{"f":"ㅐ"}], "p":[{"f":"ㅔ"}], "P":[{"f":"mod2"},{"f":"ㅔ"}], "H":[{"f":"ㅚ"}], "N":[{"f":"ㅟ"}], "M":[{"f":"ㅢ"}], "K":[{"f":"mod0"},{"f":"ㅏ"}], "Y":[{"f":"mod0"},{"f":"ㅐ"}], "J":[{"f":"mod0"},{"f":"ㅓ"}], "B":[{"f":"mod0"},{"f":"ㅔ"}], " ":[{"f":"sp","s":true}], ",":[{"f":"comma","s":true}], ".":[{"f":"period","s":true}], "?":[{"f":"que","s":true}], "!":[{"f":"exc","s":true}] }`)
+    refresh();
+}
+
+function switch2() {
+    code = JSON.parse(`{ "r":[{"f":"ㄱ"}], "s":[{"f":"ㄴ"}], "e":[{"f":"ㄷ"}], "f":[{"f":"ㄹ"}], "a":[{"f":"ㅁ"}], "q":[{"f":"ㅂ"}], "t":[{"f":"ㅅ"}], "d":[{"f":"ㅇ"}], "w":[{"f":"ㅈ"}], "c":[{"f":"ㅊ"}], "z":[{"f":"ㅋ"}], "x":[{"f":"ㅌ"}], "v":[{"f":"ㅍ"}], "g":[{"f":"ㅎ"}], "R":[{"f":"ㄲ"}], "E":[{"f":"ㄸ"}], "Q":[{"f":"ㅃ"}], "T":[{"f":"ㅆ"}], "W":[{"f":"ㅉ"}], "k":[{"f":"ㅏ"}], "i":[{"f":"mod2"},{"f":"ㅏ"}], "j":[{"f":"ㅓ"}], "u":[{"f":"mod2"},{"f":"ㅓ"}], "h":[{"f":"ㅗ"}], "y":[{"f":"mod2"},{"f":"ㅗ"}], "n":[{"f":"ㅜ"}], "b":[{"f":"mod2"},{"f":"ㅜ"}], "m":[{"f":"ㅡ"}], "l":[{"f":"ㅣ"}], "o":[{"f":"ㅐ"}], "O":[{"f":"mod2"},{"f":"ㅐ"}], "p":[{"f":"ㅔ"}], "P":[{"f":"mod2"},{"f":"ㅔ"}], "H":[{"f":"ㅚ"}], "N":[{"f":"ㅟ"}], "M":[{"f":"ㅢ"}], "K":[{"f":"mod0"},{"f":"ㅏ"}], "Y":[{"f":"mod0"},{"f":"ㅐ"}], "J":[{"f":"mod0"},{"f":"ㅓ"}], "B":[{"f":"mod0"},{"f":"ㅔ"}], " ":[{"f":"sp","s":true}], ",":[{"f":"comma","s":true}], ".":[{"f":"period","s":true}], "?":[{"f":"que","s":true}], "!":[{"f":"exc","s":true}] }`)
+    refresh();
+}
+
+function switch3() {
+    code = JSON.parse(`{ "r":[{"f":"ㄱ"}], "s":[{"f":"ㄴ"}], "e":[{"f":"ㄷ"}], "f":[{"f":"ㄹ"}], "a":[{"f":"ㅁ"}], "q":[{"f":"ㅂ"}], "t":[{"f":"ㅅ"}], "d":[{"f":"ㅇ"}], "w":[{"f":"ㅈ"}], "c":[{"f":"ㅊ2"}], "z":[{"f":"ㅋ"}], "x":[{"f":"ㅌ2"}], "v":[{"f":"ㅍ2"}], "g":[{"f":"ㅎ"}], "R":[{"f":"ㄲ2"}], "E":[{"f":"ㄸ2"}], "Q":[{"f":"ㅃ2"}], "T":[{"f":"ㅆ2"}], "W":[{"f":"ㅉ2"}], "k":[{"f":"ㅏ"}], "i":[{"f":"mod2"},{"f":"ㅏ"}], "j":[{"f":"ㅓ"}], "u":[{"f":"mod2"},{"f":"ㅓ"}], "h":[{"f":"ㅗ"}], "y":[{"f":"mod2"},{"f":"ㅗ"}], "n":[{"f":"ㅜ"}], "b":[{"f":"mod2"},{"f":"ㅜ"}], "m":[{"f":"ㅡ"}], "l":[{"f":"ㅣ"}], "o":[{"f":"ㅐ"}], "O":[{"f":"mod2"},{"f":"ㅐ"}], "p":[{"f":"ㅔ"}], "P":[{"f":"mod2"},{"f":"ㅔ"}], "H":[{"f":"ㅚ"}], "N":[{"f":"ㅟ"}], "M":[{"f":"ㅢ"}], "K":[{"f":"mod0"},{"f":"ㅏ"}], "Y":[{"f":"mod0"},{"f":"ㅐ"}], "J":[{"f":"mod0"},{"f":"ㅓ"}], "B":[{"f":"mod0"},{"f":"ㅔ"}], " ":[{"f":"sp","s":true}], ",":[{"f":"comma","s":true}], ".":[{"f":"period","s":true}], "?":[{"f":"que","s":true}], "!":[{"f":"exc","s":true}] }`)
+    refresh();
+}
+
+function refresh() {
+    oldcode = []
+    update()
+}
+
+switch2();
+update_size_slider();
+
+chcode = ['r','R','s','e','E','f','a','q','Q','t','T','d','w','W','c','z','x','v','g']
+jucode = ['k','o','i','O','j','p','u','P','h','K','Y','H','y','n','J','B','N','b','m','M','l']
+jocode = ['','r','R','rt','s','sw','sg','e','f','fr','fa','fq','ft','fx','fv','fg','a','q','qt','t','T','d','w','c','z','x','v','g']
+cscode = ['','r','R','rt','s','sw','sg','e','E','f','fr','fa','fq','ft','fx','fv','fg','a','q','Q','qt','t','T','d','w','W','c','z','x','v','g']
+
+$("#code").on("input", update)
+
+$("#result-size").mousemove(function() {
+    let news = `calc(${$("#result-size").val()/20} * var(--base-length))`
+    $(":root").css("--symbol-size", news)
+    $("#result-size-label").text(`크기: x${$("#result-size").val()/20}`)
+})
+
+$("#result-width").mousemove(function() {
+    let neww = `calc(${$("#result-width").val()} * var(--symbol-size))`
+    $(".result-container").css("width", neww)
+    $("#result-width-label").text(`너비: ${$("#result-width").val()}개`)
+    update_size_slider();
+})
+
+function update_size_slider() {
+    $("#result-width").attr("max",$("body").width() / $("#symbol-size").width() - 1)
+}
+
+function to_code(str) {
+    let res1 = str.replaceAll(', ',',').replaceAll('. ','.').replaceAll('? ','?').replaceAll('! ','!').replaceAll('ml','M').replaceAll('hk','K').replaceAll('ho','Y').replaceAll('hl','H').replaceAll('nj','J').replaceAll('np','B').replaceAll('nl','N').replaceAll('\\','')
+    let res2 = ''
+    for (ch of res1) {
+        let c = ch.charCodeAt(0)
+        if (0x1100 <= c && c <= 0x1112) {
+            c -= 0x1100
+            res2 += chcode[c]
+            continue
+        }
+        if (0x1161 <= c && c <= 0x1175) {
+            c -= 0x1161
+            res2 += jucode[c]
+            continue
+        }
+        if (0x11a8 <= c && c <= 0x11c2) {
+            c -= 0x11a7
+            res2 += jocode[c]
+            continue
+        }
+        if (0x3131 <= c && c <= 0x314e) {
+            c -= 0x3130
+            res2 += cscode[c]
+            continue
+        }
+        if (0x314f <= c && c <= 0x3163) {
+            c -= 0x314f
+            res2 += jucode[c]
+            continue
+        }
+        if (0xac00 <= c && c <= 0xd7a3) {
+            c -= 0xac00
+            let chidx = Math.floor(c / 588)
+            let juidx = Math.floor((c%588) / 28)
+            let joidx = c%28
+            res2 += chcode[chidx]
+            res2 += jucode[juidx]
+            res2 += jocode[joidx]
+            continue
+        }
+        res2 += ch
+    }
+    res3 = []
+    for (ch of res2) {
+        if (ch == '\n') {
+            res3 = res3.concat([{'f':'\n'}])
+            continue
+        }
+        if ('0'<=ch && ch<='9') {
+            res3 = res3.concat([{'f':ch}])
+            continue
+        }
+        if (code[ch] === undefined) {
+            res3 = res3.concat([{'f':'!'}])
+            continue
+        }
+        res3 = res3.concat(code[ch])
+    }
+    return res3
+}
+
+var oldcode = []
+function update() {
+    let txt = to_code($("#code").val())
+    let common = 0
+    for (i=0; i<Math.min(oldcode.length, txt.length); i++) {
+        if (JSON.stringify(oldcode[i]) != JSON.stringify(txt[i])) break
+        common++
+    }
+    console.log(common+" + \""+JSON.stringify(txt.slice(common))+"\"")
+    oldcode = txt
+    txt = txt.slice(common)
+    $(`.result-img:nth-child(n+${common+1})`).remove()
+    for (ch of txt) {
+        if (ch.f == '\n') {
+            $(".result-container").append(`<br class="result-img">`)
+            continue
+        }
+        if (ch.s) {
+            $(".result-container").append(`<div class="result-img" style="--data:url(img/${ch.f}.svg); aspect-ratio: 0.5;" />`)
+        } else {
+            $(".result-container").append(`<div class="result-img" style="--data:url(img/${ch.f}.svg);" />`)
+        }
+    }
+}
