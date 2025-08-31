457edc66d20b233cc87f3bd96fa125d6a448b727
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-08-31 20:26:15 +0900

    change things + add the archive switch

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 var clicks = 0;
 function logo_click(e) {
+    if (clicks >= 6) return;
     e.style.animation='none';
     e.offsetHeight;
     e.style.animation='1s test-rotate';
     clicks++;
     console.log(`ecyc ${clicks}`);
     if (clicks<6) {
-        window.setTimeout(function() {
-            if (clicks < 6) {
-                clicks = 0;
-            }
-        }, 1000);
+        window.setTimeout(() => { if (clicks < 6) clicks = 0; }, 1000);
+        return;
     } else {
-        if (clicks%4==2) {
-            $("#logo-img").removeClass("tilted");
-            $("#title").text("rlachi web");
-            $(".hidden-site").removeClass("hidden");
-        } else if (clicks%4==0) {
-            $("#logo-img").addClass("tilted");
-            $("#title").text("kimcl web");
-            $(".hidden-site").addClass("hidden");
-        }
+        $("#logo-img").removeClass("tilted");
+        $("#title").text("rlachi web");
+        $(".hidden-site").removeClass("hidden");
     }
 }
