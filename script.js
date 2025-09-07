0fdd882e2980c03221875cd265b8373103ed1d79
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-09-07 13:52:28 +0900

    oh this makes code more ggalggeum

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ function logo_click(e) {
     console.log(`ecyc ${clicks}`);
     if (clicks<6) {
         if (killcode!==null) window.clearTimeout(killcode);
-        killcode = window.setTimeout(() => { if (clicks < 6) clicks = 0; }, 1000);
+        killcode = window.setTimeout(() => { clicks = 0; }, 1000);
         return;
     } else {
+        if (killcode!==null) window.clearTimeout(killcode);
         $("#logo-img").removeClass("tilted");
         $("#title").text("rlachi web");
         $(".hidden-site").removeClass("hidden");
