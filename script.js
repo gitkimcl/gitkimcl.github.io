ddca26e16f3d1b1a9e45bcc943dad20df4d64eaf
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-09-07 14:33:37 +0900

    it was imperfect

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var clicks = 0;
 var hue = 0;
 var prevhue = 0;
 function logo_click(e) {
-    if (prevhue==hue) hue += 360;
-    window.setTimeout(() => { prevhue = hue; }, 500);
+    if (prevhue==hue) {
+        hue += 360;
+        window.setTimeout(() => { prevhue = hue; }, 500);
+    }
     $("#logo-img").css("filter", `hue-rotate(${hue}deg)`);
     clicks++;
     console.log(`ecyc ${clicks}`);
