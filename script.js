6fb642e67c3d9f494e03a8a1070740f4b07afba1
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-09-07 13:57:03 +0900

    revert duh

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 var clicks = 0;
-var killcode = null;
 function logo_click(e) {
-    if (clicks >= 6) return;
     e.style.animation='none';
     e.offsetHeight;
     e.style.animation='1s test-rotate';
     clicks++;
     console.log(`ecyc ${clicks}`);
     if (clicks<6) {
-        if (killcode!==null) window.clearTimeout(killcode);
-        killcode = window.setTimeout(() => { clicks = 0; }, 1000);
+        killcode = window.setTimeout(() => { if (clicks<6) clicks = 0; }, 1000);
         return;
     } else {
-        if (killcode!==null) window.clearTimeout(killcode);
         $("#logo-img").removeClass("tilted");
         $("#title").text("rlachi web");
         $(".hidden-site").removeClass("hidden");
