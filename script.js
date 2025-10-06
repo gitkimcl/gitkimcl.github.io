b88eea5fb820e340f344fcab18da70cf49573665
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-10-06 17:27:13 +0900

    oops missed one

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var clicks = 0;
 function logo_click() {
-    $("#logo-img").css("animation", "none");
-    $("#logo-img").offset();
-    $("#logo-img").css("animation", "1s hue-rotate");
-    clicks++;
-    console.log(`ecyc ${clicks}`);
-    if (clicks>6) return;
-    if (clicks==6) {
-        $("#logo-img").removeClass("tilted");
-        $("#title").text("rlachi web");
-        $(".hidden-site").removeClass("hidden");
-        $(".hidden-site").removeAttr("hidden");
-        return;
-    }
-    window.setTimeout(() => { if (clicks<6) clicks = 0; }, 1000);
+	$("#logo-img").css("animation", "none");
+	$("#logo-img").offset();
+	$("#logo-img").css("animation", "1s hue-rotate");
+	clicks++;
+	console.log(`ecyc ${clicks}`);
+	if (clicks>6) return;
+	if (clicks==6) {
+		$("#logo-img").removeClass("tilted");
+		$("#title").text("rlachi web");
+		$(".hidden-site").removeClass("hidden");
+		$(".hidden-site").removeAttr("hidden");
+		return;
+	}
+	window.setTimeout(() => { if (clicks<6) clicks = 0; }, 1000);
 }
