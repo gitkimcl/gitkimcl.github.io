c45b03ed251ac6882bb663d7dc3d8863fb3343da
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-10-16 21:49:18 +0900

    kserver-menu

---
+++ things/kimclserver/global/kservermenu.js
@@ -0,0 +1,30 @@
+class KServerMenuElement extends HTMLElement {
+	#sr = null;
+	
+	constructor() {
+		super();
+		this.#sr = this.attachShadow({mode:'open'});
+	}
+	
+	connectedCallback() {
+		fetch('/things/kimclserver/global/kservermenu.html')
+			.then((res) => {
+				return res.text();
+			})
+			.then((text) => {
+				this.#sr.innerHTML = text.match(/(?<=<template>).*?(?=<\/template>)/s);
+				let sc = this.#sr.getElementById('script');
+				let nsc = document.createElement('script');
+				nsc.type = "module";
+				nsc.append(sc.innerHTML);
+				this.#sr.replaceChild(nsc, sc);
+				return;
+			})
+			.catch((err) => {
+				console.log(err);
+				this.#sr.innerHTML = `error lol`;
+			});
+	}
+}
+
+customElements.define('kserver-menu', KServerMenuElement);
