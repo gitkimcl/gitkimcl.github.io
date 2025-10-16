849bba3d2579c2dc8f37737d268fa17b09dcb448
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-10-16 16:37:41 +0900

    kimclserver

---
+++ things/kimclserver/script.js
@@ -0,0 +1,15 @@
+var clicks = 0;
+function logo_click() {
+	$("#logo-img").css("animation", "none");
+	$("#logo-img").offset();
+	$("#logo-img").css("animation", "1s hue-rotate");
+	clicks++;
+	console.log(`ecyc ${clicks}`);
+	if (clicks>6) return;
+	if (clicks==6) {
+		$("#logo-img").removeClass("tilted");
+		$("#title").text("rlachi server");
+		return;
+	}
+	window.setTimeout(() => { if (clicks<6) clicks = 0; }, 1000);
+}
