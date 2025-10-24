947e6e26a255fa5b82ea2e3153af46a50b448f1a
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-10-24 16:09:31 +0900

    diag diag(diagonal a-nim dialogim)

--- kimclserver/things/cycelog/script.js
+++ kimclserver/things/cycelog/script.js
@@ -16,3 +16,106 @@ $("q.star").prepend("★").append("★");;
 $("span.ref.paren").prepend("(").append(" )");
 $("s.namu, span.namu").append("(...)");
 $("span.qm").text("?");
+
+var cur_el = null;
+var cur_box = null;
+function move_to(el, clicky) {
+	$("#dialog").css("opacity",0).addClass("closing").removeAttr("id");
+	let rects = el.getClientRects();
+	let rect = rects[cur_box ?? 0];
+	if (clicky != null) {
+		let new_box = null;
+		for (let i in rects) {
+			let e = rects[i];
+			if (e.top <= clicky && clicky <= e.bottom) {
+				new_box = i;
+				rect = rects[i];
+				break;
+			}
+		}
+		if (cur_el === el && cur_box === new_box) {
+			$("#dialog").css("opacity",0).addClass("closing").removeAttr("id");
+			cur_el = null;
+			cur_box = null;
+			return;
+		}
+		cur_box = new_box;
+	}
+	cur_el = el;
+	let x = window.scrollX + rect.x + rect.width/2;
+	let y = window.scrollY + rect.bottom + 4;
+	create_dialog(el);
+	$("#dialog").css("left", `${x}px`).css("top",`${y}px`);
+	$("#dialog").css({
+		'--c': $(el).css('--c'),
+		'--ch': $(el).css('--ch'),
+		'--ca': $(el).css('--ca'),
+		'--bd': $(el).css('--bd'),
+		'--bdh': $(el).css('--bdh'),
+		'--bda': $(el).css('--bda'),
+		'--bg': $(el).css('--bg'),
+		'--bgh': $(el).css('--bgh'),
+		'--bga': $(el).css('--bga')
+	});
+}
+
+function create_dialog(e) {
+	let el = $(e);
+	if (e.tagName === 'A') {
+		if (el.hasClass("add")) {
+			$("body").append($(`<dialog id="dialog" open closedby="any" onclose="remove_dialog(this);" ontransitionend="on_trans(this);">
+				수첩 기록 시엔 없었으나<br>
+				${(el.hasClass("add3")?"3차 기록 중":(el.hasClass("add1")?"1차 기록 중":"이후"))} 추가한 내용
+			</dialog>`));
+			return;
+		}
+		if (el.hasClass("ref")) {
+			$("body").append($(`<dialog id="dialog" open closedby="any" onclose="remove_dialog(this);" ontransitionend="on_trans(this);">
+				${el.attr("data-for")}번 기록 언급<br>
+				<button type="button">이동</button><button command="close" commandfor="dialog" type="button">닫기</button>
+			</dialog>`));
+			return;
+		}
+		$("body").append($(`<dialog id="dialog" open closedby="any" onclose="remove_dialog(this);" ontransitionend="on_trans(this);">
+			${el.attr("data-for")}번 기록
+		</dialog>`));
+		return;
+	}
+	if (el.hasClass("qm")) {
+		$("body").append($(`<dialog id="dialog" open closedby="any" onclose="remove_dialog(this);" ontransitionend="on_trans(this);">
+			${el.attr("data-why") ?? "진위 여부나 단어 선택의 적절성이 확실하지 않음"}
+		</dialog>`));
+		return;
+	}
+	if (el.hasClass("person")) {
+		$("body").append($(`<dialog id="dialog" open closedby="any" onclose="remove_dialog(this);" ontransitionend="on_trans(this);">
+			${el.val()} · ${"대충 이름"}<br>
+			<button type="button">더 보기</button><button command="close" commandfor="dialog" type="button">닫기</button>
+		</dialog>`));
+		return;
+	}
+	$("body").append($(`<dialog id="dialog" open closedby="any" onclose="remove_dialog(this);" ontransitionend="on_trans(this);">
+		???
+	</dialog>`));
+	return;
+}
+
+function remove_dialog(e) {
+	if (e.id !== "dialog") return;
+	$(e).css("opacity",0).addClass("closing").removeAttr("id");
+	cur_el = null;
+	cur_box = null;
+}
+
+function on_trans(e) {
+	let el = $(e);
+	if (el.hasClass("closing")) el.remove();
+}
+
+$("a, data, time, span.qm").on("click", (e) => {
+	move_to(e.currentTarget, e.clientY);
+});
+
+$(window).on("resize", () => {
+	if (cur_el) move_to(cur_el);
+});
