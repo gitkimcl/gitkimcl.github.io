ed682c83829ecf7ddb3ac88d6aa847e2cce2eff6
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2025-10-25 16:43:44 +0900

    dialog code jeongli

--- kimclserver/things/cycelog/script.js
+++ kimclserver/things/cycelog/script.js
@@ -23,7 +23,7 @@ $("a, data, time, span.qm").on("click", (e) => {
 
 $(window).on("resize", () => {
 	if (!cur_el) return;
-	actually_move(cur_el);
+	actually_move();
 });
 
 var cur_el = null;
@@ -31,114 +31,89 @@ var cur_box = null;
 function move_to(el, clicky) {
 	$("#dialog").css("opacity",0).addClass("closing").removeAttr("id");
 	let rects = el.getClientRects();
-	let rect = rects[cur_box ?? 0];
 	if (clicky != null) {
 		let new_box = null;
 		for (let i in rects) {
 			let e = rects[i];
 			if (e.top <= clicky && clicky <= e.bottom) {
 				new_box = i;
-				rect = rects[i];
 				break;
 			}
 		}
 		if (cur_el === el && cur_box === new_box) {
 			$("#dialog").css("opacity",0).addClass("closing").removeAttr("id");
-			cur_el = null;
-			cur_box = null;
+			cur_el = cur_box = null;
 			return;
 		}
 		cur_box = new_box;
 	}
 	cur_el = el;
-	create_dialog(el);
-	actually_move(el);
-	$("#dialog").css({
-		'--c': $(el).css('--c'),
-		'--ch': $(el).css('--ch'),
-		'--ca': $(el).css('--ca'),
-		'--bd': $(el).css('--bd'),
-		'--bdh': $(el).css('--bdh'),
-		'--bda': $(el).css('--bda'),
-		'--bg': $(el).css('--bg'),
-		'--bgh': $(el).css('--bgh'),
-		'--bga': $(el).css('--bga')
-	});
+	create_dialog();
+	actually_move();
+	$("#dialog").css($(el).css(['--c','--ch','--ca','--bd','--bdh','--bda','--bg','--bgh','--bga']));
 }
 
-function actually_move(el) {
-	let rects = el.getClientRects();
-	let rect = rects[cur_box ?? 0];
+function actually_move() {
+	let rect = cur_el.getClientRects()[cur_box ?? 0];
 	let x = window.scrollX + rect.x + rect.width/2;
 	let y = window.scrollY + rect.bottom + 4;
 	let vx = window.innerWidth;
 	let dw = $("#dialog").innerWidth();
 	if (x < dw/2+12) x = dw/2 + 12;
 	if (x > vx-dw/2-12) x = vx-dw/2 - 12;
-	console.log(`${x}`);
 	$("#dialog").css("left", `${x}px`).css("top",`${y}px`);
 }
 
-function create_dialog(e) {
-	let el = $(e);
-	if (e.tagName === 'A') {
+function dialog_base() {
+	return $(`<dialog id="dialog" open onclick="event.stopPropagation();" ontransitionend="on_trans(this);"></dialog>`);
+}
+
+function create_dialog() {
+	let el = $(cur_el);
+	if (cur_el.tagName === 'A') {
 		if (el.hasClass("add")) {
-			$("body").append($(`<dialog id="dialog" open onclick="event.stopPropagation();" onclose="remove_dialog(this);" ontransitionend="on_trans(this);">
-				<span>수첩 기록 시엔 없었으나<br>
-				${(el.hasClass("add3")?"3차 기록 중":(el.hasClass("add1")?"1차 기록 중":"이후"))} 추가한 내용</span>
-			</dialog>`));
+			$("body").append(dialog_base().append(`<span>수첩 기록 시엔 없었으나<br>
+				${(el.hasClass("add3")?"3차 기록 중":(el.hasClass("add1")?"1차 기록 중":"이후"))} 추가한 내용</span>`));
 			return;
 		}
 		if (el.hasClass("ref")) {
-			$("body").append($(`<dialog id="dialog" open onclick="event.stopPropagation();" ontransitionend="on_trans(this);">
-				<span>${el.attr("data-for")}번 기록 언급<br></span>
-				<button type="button" onclick="event.stopPropagation();">이동</button>
-			</dialog>`));
+			$("body").append(dialog_base().append(`<span>${el.attr("data-for")}번 기록 언급<br></span>
+				<button type="button" onclick="event.stopPropagation();">이동</button>`));
 			return;
 		}
-		$("body").append($(`<dialog id="dialog" open onclick="event.stopPropagation();" ontransitionend="on_trans(this);">
-			<span>${el.attr("data-for")}번 기록</span>
-		</dialog>`));
+		$("body").append(dialog_base().append(`<span>${el.attr("data-for")}번 기록</span>`));
 		return;
 	}
 	if (el.hasClass("qm")) {
-		$("body").append($(`<dialog id="dialog" open onclick="event.stopPropagation();" ontransitionend="on_trans(this);">
-			<span>${el.attr("data-why") ?? "진위 여부나 단어 선택의 적절성이 확실하지 않음"}</span>
-		</dialog>`));
+		$("body").append(dialog_base().append(`<span>${el.attr("data-why") ?? "진위 여부나 단어 선택의 적절성이 확실하지 않음"}</span>`));
 		return;
 	}
 	if (el.hasClass("person")) {
-		$("body").append($(`<dialog id="dialog" open onclick="event.stopPropagation();" ontransitionend="on_trans(this);">
-			<span>${el.val()} · ${"대충 이름"}<br></span>
-			<button type="button" onclick="event.stopPropagation();">더 보기</button>
-		</dialog>`));
+		$("body").append(dialog_base().append(`<span>${el.val()} · ${"대충 이름"}<br></span>
+			<button type="button" onclick="event.stopPropagation();">더 보기</button>`));
 		return;
 	}
 	if (el.hasClass("placeholder")) {
-		$("body").append($(`<dialog id="dialog" open onclick="event.stopPropagation();" ontransitionend="on_trans(this);">
-			<span>텍스트로 나타낼 수 없어 대체된 내용</span>${el.val() && `<br>
-			<span>${el.val()}</span>`}
-		</dialog>`));
+		$("body").append(dialog_base().append(`<span>텍스트로 나타낼 수 없어 대체된 내용</span>
+			${el.val() && `<br>
+			<span>${el.val()}</span>`}`));
 		return;
 	}
-	$("body").append($(`<dialog id="dialog" open onclick="event.stopPropagation();" ontransitionend="on_trans(this);">
-		<span>???</span>
-	</dialog>`));
+	$("body").append(dialog_base().append(`<span>???</span>`));
 	return;
 }
 
 $("body").on("click", (_) => {
+	if (!document.getElementById("dialog")) return;
 	remove_dialog(document.getElementById("dialog"));
 })
 
 function remove_dialog(e) {
 	if (e.id !== "dialog") return;
 	$(e).css("opacity",0).addClass("closing").removeAttr("id");
-	cur_el = null;
-	cur_box = null;
+	cur_el = cur_box = null;
 }
 
 function on_trans(e) {
-	let el = $(e);
-	if (el.hasClass("closing")) el.remove();
+	if (e.classList.contains("closing")) e.remove();
 }
